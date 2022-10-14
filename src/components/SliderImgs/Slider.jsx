5c2efe680df9ddce662ecab131489ca80ab38795
MohammedAlghazali <mohammed.m.alghazali@hotmail.com>
2022-10-14 13:10:39 +0300

    edit banner imgs

--- src/components/SliderImgs/Slider.jsx
+++ src/components/SliderImgs/Slider.jsx
@@ -3,7 +3,7 @@ import BtnSlider from './BtnSlider';
 import dataSlider from './dataSlider';
 import visual from '../../assets/visual.png';
 import { useNavigate } from 'react-router-dom';
-// import visual2 from '../../assets/visual2.png';
+import visual2 from '../../assets/visual2.png';
 
 export default function Slider() {
 
@@ -55,7 +55,7 @@ export default function Slider() {
                             : <div
                                 key={obj.id}
                                 className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
-                                style={{ background: `url(${visual}) 77.625rem 29.6875rem round` }}
+                                style={{ background: `url(${visual2}) 77.625rem 29.6875rem round` }}
                                 onClick={() => navigate('/live-casino')}
                             >
                                 {/* {slideIndex === 1 ? <p style={{ postion: 'absolute' }}>다양하고 멋진 경기들이 준비되어 있습니다. <br /> 더 쉽고 재미있는 스포츠베팅의 세계를 경험하세요!</p> : null} */}
