aad46b3ba006db5871efe9fe3722e4fe978469e7
MohammedAlghazali <mohammed.m.alghazali@hotmail.com>
2022-10-20 14:29:12 +0300

    .

--- src/components/ContactView/index.jsx
+++ src/components/ContactView/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import Rank1 from '../../assets/myPage/freeboard/1.png';
 import parse from 'html-react-parser';
