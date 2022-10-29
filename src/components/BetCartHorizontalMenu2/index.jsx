87117a964b8584f89e71baf6f7e6693f9c245102
MohammedAlghazali <mohammed.m.alghazali@hotmail.com>
2022-10-29 18:17:35 +0200

    .

--- src/components/BetCartHorizontalMenu2/index.jsx
+++ src/components/BetCartHorizontalMenu2/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 import activeBG from '../../assets/betCombinationRightPanel/active-bg.png'
--- src/components/HorizontalMenu9/index.jsx
+++ src/components/HorizontalMenu9/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router';
 import activeBtn from '../../assets/sports/active-btn.png'
