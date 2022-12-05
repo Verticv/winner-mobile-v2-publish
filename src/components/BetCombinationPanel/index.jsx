b6b213f31fde39b4a7fd412f74488f4e642d2530
Andrew Jang <dhj9817@gmail.com>
2022-12-05 20:36:44 +0900

    .

--- src/components/BetCombinationPanel/index.jsx
+++ src/components/BetCombinationPanel/index.jsx
@@ -1156,7 +1156,7 @@ const BetCombinationPanel = ({
                                     }}
                                     className="leaguecell-content5"
                                 >
-                                    <span style={{ textShadow: "1px 1px 1px #00000070", marginTop: isOpen[items.id] === true ? '-0.2rem' : '0.3rem' }} className="leaguecell-content6" >
+                                    <span style={{ textShadow: "1px 1px 1px #00000070", marginTop: isOpen[items.id] === true ? '-0.4rem' : '0.3rem' }} className="leaguecell-content6" >
                                         {isOpen[items.id] === true ? (
                                             <img className='leaguecell-content7' src={Minus} alt="" />
                                         ) : (
